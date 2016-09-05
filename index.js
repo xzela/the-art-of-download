df4b9ab3ee8158bfd56485b3a0fcdf225e0f50b5
xzela <zeph@lafassett.com>
2016-09-04 17:38:00 -0700

    done?

--- index.js
+++ index.js
@@ -1,60 +1,107 @@
 'use strict';
 
-var // request = require('request'),
+var cluster = require('cluster'),
 	http = require('http'),
-	nopt = require("nopt"),
-	ProgressBar = require('progress'),
-	fs = require('fs');
-// http://:@theartofblowjob.com/content/2007-05-21.zip
-var date = '2009-05-07';
-var uri = "http://@theartofblowjob.com/content/" + date + '.zip';
-var known = {
-		'username': String,
-		'password': String
-	},
-	short = {
-		'u': ['--username'],
-		'p': ['--password']
-	};
-var parsed = nopt(known, short, process.argv, 2);
-// console.log(parsed);
+	fs = require('fs'),
+	cpus = require('os').cpus();
 
-// process.exit();
+// first date to start with
 
-// request.get(url + '/content/' + date + '.zip').pipe(fs.createWriteStream('./downloads/' + date + '.zip'));
-var req = http.get(uri, function (response) {
-	var len = parseInt(response.headers['content-length'], 10);
-	var bar = new ProgressBar('  downloading [:bar] :percent :etas', {
-		complete: '=',
-		incomplete: ' ',
-		width: 20,
-		total: len
+var username = process.argv[2];
+var password = process.argv[3];
+var firstDate = process.argv[4] || '2009-01-01';
+
+if (username === undefined || password === undefined) {
+	throw new Error('`username` and `password` are required!');
+}
+
+var date = firstDate;
+var dates = [];
+
+// helper function to add day to a date
+function addDays(date, num) {
+	var n = new Date(date);
+	n.setDate(n.getDate() + num);
+	return n;
+}
+// populate the date list
+while(Date.parse(new Date(date)) <= Date.parse(new Date())) {
+	date = addDays(date, 1);
+	dates.push(date.toISOString().substr(0, 10));
+}
+if (cluster.isMaster) {
+	// Fork workers by cpu halves.
+	for (var i = 0; i < cpus.length / 2; i++) {
+		cluster.fork();
+	}
+
+	cluster.on('online', (worker) => {
+		console.log(`${worker.id} is online!`);
 	});
 
-	var body = "";
-	var cur = 0;
+	cluster.on('message', (worker) => {
+		if (dates.length <= 0) {
+			console.log(`worker: ${worker.id} has nothing to do!`);
+			worker.disconnect();
+			return;
+		}
+		// console.log('date length is:', dates.length, msg);
+		var file = dates.pop() + '.zip';
+		var uri = '/content/' + file;
+		var options = {
+			protocol: 'http:',
+			auth: username + ':' + password,
+			method: 'GET',
+			host: 'theartofblowjob.com',
+			path: uri,
+		};
 
-	response.on('data', function (chunk) {
-		bar.tick(chunk.length);
+		var callback = function (response) {
+
+			if (response.statusCode !== 200) {
+				worker.disconnect();
+				return;
+			}
+
+			var len = parseInt(response.headers['content-length'], 10);
+			var cur = 0;
+			console.log(`${worker.id} downloading file ${file}`);
+			var opts = {
+				flags: 'w',
+				efaultEncoding: 'binary'
+			};
+			var dest = fs.createWriteStream('./downloads/' + file, opts);
+
+			response.on('data', (chunk) => {
+				if (!chunk) {
+					return;
+				}
+				dest.write(chunk);
+				cur += chunk.length;
+				var percent = (100 * cur / len).toFixed(2);
+				console.log(worker.id, file, percent);
+			});
+
+			response.on('end', () => {
+				worker.disconnect();
+			});
+
+			dest.on('end', () => {
+				// worker.send({message: 'try again'});
+			});
+		};
+		http.get(options, callback);
 	});
 
-	response.on('data2', function (chunk) {
-		body += chunk;
-		cur += chunk.length;
-		var percent = (100 * cur / len).toFixed(2);
-		console.log(percent);
-		if (percent % 20 === 0) {
-			console.log('20 precent more ');
+	cluster.on('exit', (dead) => {
+		console.log(`${dead.id} has died`);
+		if (dates.length > 0) {
+			console.log(`more work to do spinning up new worker....`);
+			var worker = cluster.fork();
+			console.log(`${worker.id} has been born`);
 		}
 	});
 
-	response.on('end', function () {
-		fs.writeFile('./downloads/' + date + '.zip', body, {}, function () {
-			console.log('done!');
-		});
-	});
-
-	response.on('error', function (err) {
-		console.error(err);
-	});
-});
+} else {
+	process.send({message: 'download'});
+}
